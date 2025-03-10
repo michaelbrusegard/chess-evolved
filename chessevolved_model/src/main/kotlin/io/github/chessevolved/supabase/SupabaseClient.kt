03338ba820b03e3e68d532706e20496c4d0254bf
Sivert Underdal <sivertun@stud.ntnu.no>
2025-03-11 00:24:58 +0100

    refactor: refactor SupabaseClient to return the client in a function

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/supabase/SupabaseClient.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/supabase/SupabaseClient.kt
@@ -1,5 +1,6 @@
 package io.github.chessevolved.supabase
 
+import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.logging.LogLevel
 import io.github.jan.supabase.postgrest.Postgrest
@@ -9,22 +10,23 @@ import kotlinx.serialization.json.Json
 import kotlin.time.Duration.Companion.seconds
 
 object SupabaseClient {
-    init {
-        createSupabaseClient(
-            supabaseUrl = "https://tsmubattgglbqaarktnw.supabase.co",
+    private val supabase : SupabaseClient = createSupabaseClient(
+        supabaseUrl = "https://tsmubattgglbqaarktnw.supabase.co",
 
-            // This key is an admin (service role) key.
-            supabaseKey = ""
-        ) {
-            install(Postgrest)
-            install(Realtime) {
-                reconnectDelay = 5.seconds // Default: 7 seconds
-            }
-            defaultLogLevel = LogLevel.INFO
-            defaultSerializer = KotlinXSerializer(Json {
-                //Custom serializer config
-            })
+        // This key is an admin (service role) key.
+        supabaseKey = ""
+    ) {
+        install(Postgrest)
+        install(Realtime) {
+            reconnectDelay = 5.seconds // Default: 7 seconds
         }
-        println("Supabase Client Initialized")
+        defaultLogLevel = LogLevel.INFO
+        defaultSerializer = KotlinXSerializer(Json {
+            //Custom serializer config
+        })
+    }
+
+    public fun getSupabaseClient(): SupabaseClient {
+        return supabase
     }
 }
