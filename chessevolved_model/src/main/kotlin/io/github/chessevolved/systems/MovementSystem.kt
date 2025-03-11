3a61b9da39d3bc26b347c61536bb5e6fac49406e
William Tjonndal <williaht@stud.ntnu.no>
2025-03-11 10:23:35 +0100

    feat: update moveChessPiece, remove unused imports
    moveChessPiece is rather simple now, need to refactor when the ComponentMapper class is created

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/systems/MovementSystem.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/systems/MovementSystem.kt
@@ -1,11 +1,15 @@
 package io.github.chessevolved.systems
 
+import com.badlogic.ashley.core.ComponentMapper
 import io.github.chessevolved.components.PositionComponent
 import io.github.chessevolved.entities.ChessPiece
-import ktx.ashley.get
 
 class MovementSystem {
-    fun moveChessPiece(chesspiece: ChessPiece, x: Int, y: Int): Unit {
-
+    //TODO: refactor when the ComponentMapper class is created
+    fun moveChessPiece(chesspiece: ChessPiece, x:Int, y:Int) {
+        val pm:ComponentMapper<PositionComponent> = ComponentMapper.getFor(PositionComponent::class.java)
+        val position:PositionComponent = pm.get(chesspiece)
+        // 1 tile has a size of 32 by 32 pixels
+        position.changePosition(x, y)
     }
 }
