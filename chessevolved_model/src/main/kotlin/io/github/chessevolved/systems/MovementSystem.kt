29659c509ca54f66dc0f9fd904ebf05b728a2a50
William Tjonndal <williaht@stud.ntnu.no>
2025-03-11 09:35:01 +0100

    Create MovementSystem.kt class
    Currently contains an empty function changeChessPiecePosition

---
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/systems/MovementSystem.kt
@@ -0,0 +1,11 @@
+package io.github.chessevolved.systems
+
+import io.github.chessevolved.components.PositionComponent
+import io.github.chessevolved.entities.ChessPiece
+import ktx.ashley.get
+
+class MovementSystem {
+    fun changeChessPiecePosition(chesspiece: ChessPiece, x: Int, y: Int): Unit {
+
+    }
+}
