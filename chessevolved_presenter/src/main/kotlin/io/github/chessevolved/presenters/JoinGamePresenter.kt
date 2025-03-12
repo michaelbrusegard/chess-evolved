03dfb23c2bb8358860ecacf93e4fcbeaacdc9c0d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-12 10:56:20 +0100

    debug: add prints to presenter

--- chessevolved_presenter/src/main/kotlin/io/github/chessevolved/presenters/JoinGamePresenter.kt
+++ chessevolved_presenter/src/main/kotlin/io/github/chessevolved/presenters/JoinGamePresenter.kt
@@ -13,10 +13,26 @@ class JoinGamePresenter(
      * @param lobbyID String representing the lobby to join
      * @return Boolean indicating success or failure
      */
-    fun joinGame(lobbyID: String): Boolean = lobbySystem.joinLobby(lobbyID)
+    fun joinGame(lobbyID: String): Boolean {
+        println("JoinGamePresenter: Attempting to join lobby with ID: $lobbyID")
+
+        val success = lobbySystem.joinLobby(lobbyID)
+
+        if (success) {
+            println("JoinGamePresenter: Successfully joined lobby $lobbyID")
+            view.showJoinSuccess()
+        } else {
+            println("JoinGamePresenter: Failed to join lobby $lobbyID")
+            view.showJoinError("Unable to join lobby: $lobbyID")
+        }
+
+        return success
+    }
 
     fun onJoinButtonPressed() {
+        println("JoinGamePresenter: Join button pressed")
         val lobbyId = view.getLobbyId()
+        println("JoinGamePresenter: Retrieved lobby ID from view: '$lobbyId'")
         joinGame(lobbyId)
     }
 
@@ -24,12 +40,16 @@ class JoinGamePresenter(
      * Return to the menu scene using the presenter manager??
      */
     fun returnToMenu() {
+        println("JoinGamePresenter: Returning to menu")
         view.closeView()
+        println("JoinGamePresenter: View closed")
         // TODO: idk but this should return here to let another presenter handle it
         // presenterManager.popToPresenter(MenuPresenter::class)
     }
 
     override fun render() {
+        println("JoinGamePresenter: Rendering join game view")
         view.displayLobbyInput()
+        println("JoinGamePresenter: Join game view displayed")
     }
 }
