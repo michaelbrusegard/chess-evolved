9d0f2d8fe621437134d786b33fc080fba18d8c9d
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-12 10:55:57 +0100

    feat: work on view for join game presenter

--- chessevolved_presenter/src/main/kotlin/io/github/chessevolved/presenters/JoinGamePresenter.kt
+++ chessevolved_presenter/src/main/kotlin/io/github/chessevolved/presenters/JoinGamePresenter.kt
@@ -1,14 +1,12 @@
 package io.github.chessevolved.presenters
 
 import io.github.chessevolved.systems.LobbySystem
-import io.github.chessevolved.views.IView
+import io.github.chessevolved.views.JoinGameView
 
 class JoinGamePresenter(
-    joinGameView: IView? = null,
+    private val view: JoinGameView,
     private val lobbySystem: LobbySystem = LobbySystem(),
 ) : IPresenter {
-    val view: IView? = joinGameView
-
     /**
      * Attempts to join a game lobby by ID
      *
@@ -17,29 +15,21 @@ class JoinGamePresenter(
      */
     fun joinGame(lobbyID: String): Boolean = lobbySystem.joinLobby(lobbyID)
 
+    fun onJoinButtonPressed() {
+        val lobbyId = view.getLobbyId()
+        joinGame(lobbyId)
+    }
+
     /**
-     * Return to the menu scene using the presenter stack
+     * Return to the menu scene using the presenter manager??
      */
     fun returnToMenu() {
+        view.closeView()
         // TODO: idk but this should return here to let another presenter handle it
         // presenterManager.popToPresenter(MenuPresenter::class)
     }
 
-    /**
-     * Get the current state of the join game view
-     *
-     * @return Map containing view state information
-     */
-    fun getJoinGameState(): Map<String, Any> =
-        mapOf(
-            "title" to "Join Game",
-            "placeholderText" to "Enter Lobby ID",
-            // Add more state information as needed by the view
-        )
-
     override fun render() {
-        // Required by IPresenter
-        // The view might need to show a lobby ID input field and buttons
-        // This will be implemented when the view is ready
+        view.displayLobbyInput()
     }
 }
---
+++ chessevolved_view/src/main/kotlin/io/github/chessevolved/views/JoinGameView.kt
@@ -0,0 +1,35 @@
+package io.github.chessevolved.views
+
+interface JoinGameView {
+    fun displayLobbyInput()
+    fun showJoinSuccess()
+    fun showJoinError(message: String)
+    fun getLobbyId(): String
+    fun closeView()
+}
+
+class JoinGameViewImpl : JoinGameView {
+    private var lobbyIdInput: String = ""
+    
+    override fun displayLobbyInput() {
+        // Setup UI for lobby input
+    }
+    
+    override fun showJoinSuccess() {
+        // Show success state
+    }
+    
+    override fun showJoinError(message: String) {
+        // Show error with message
+    }
+    
+    override fun getLobbyId(): String = lobbyIdInput
+    
+    fun setLobbyId(id: String) {
+        lobbyIdInput = id
+    }
+    
+    override fun closeView() {
+        // Handle view closing
+    }
+}
