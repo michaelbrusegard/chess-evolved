63f23cad63e927ac016172e56788051607b6ceb2
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-12 11:40:36 +0100

    chore: add comment on temp view

--- chessevolved_view/src/main/kotlin/io/github/chessevolved/views/JoinGameView.kt
+++ chessevolved_view/src/main/kotlin/io/github/chessevolved/views/JoinGameView.kt
@@ -2,33 +2,38 @@ package io.github.chessevolved.views
 
 interface JoinGameView {
     fun displayLobbyInput()
+
     fun showJoinSuccess()
+
     fun showJoinError(message: String)
+
     fun getLobbyId(): String
+
     fun closeView()
 }
 
+// This is temporary for now
 class JoinGameViewImpl : JoinGameView {
     private var lobbyIdInput: String = ""
-    
+
     override fun displayLobbyInput() {
         // Setup UI for lobby input
     }
-    
+
     override fun showJoinSuccess() {
         // Show success state
     }
-    
+
     override fun showJoinError(message: String) {
         // Show error with message
     }
-    
+
     override fun getLobbyId(): String = lobbyIdInput
-    
+
     fun setLobbyId(id: String) {
         lobbyIdInput = id
     }
-    
+
     override fun closeView() {
         // Handle view closing
     }
