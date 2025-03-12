887f89d629881a4244ca00453fcb46d14653b074
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-12 11:37:16 +0100

    Revert "chore: work on testing join game view"
    This reverts commit 6d87169049ef7e3726033aefdab929a55caedf95.

--- chessevolved_presenter/src/main/kotlin/io/github/chessevolved/ChessEvolvedGame.kt
+++ chessevolved_presenter/src/main/kotlin/io/github/chessevolved/ChessEvolvedGame.kt
@@ -1,23 +1,18 @@
 package io.github.chessevolved
 
-import com.badlogic.gdx.Gdx
-import com.badlogic.gdx.InputAdapter
 import io.github.chessevolved.presenters.GamePresenter
-import io.github.chessevolved.presenters.JoinGamePresenter
 import io.github.chessevolved.views.AndroidView
-import io.github.chessevolved.views.JoinGameViewImpl
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
 import ktx.async.KtxAsync
 
-class ChessEvolvedGame : KtxGame<KtxScreen>() {
+class ChessEvolvedGame : KtxGame<KtxScreen>(){
     override fun create() {
         KtxAsync.initiate()
 
-        // addScreen(FirstScreen())
-        addScreen(JoinGameScreen())
-        setScreen<JoinGameScreen>()
+        addScreen(FirstScreen())
+        setScreen<FirstScreen>()
     }
 }
 
@@ -28,60 +23,5 @@ class FirstScreen : KtxScreen {
     override fun render(delta: Float) {
         clearScreen(red = 0.1f, green = 0.1f, blue = 0.23f)
         presenter.render()
-    }
-}
-
-// Testing join game view
-class JoinGameScreen : KtxScreen {
-    private val view: JoinGameViewImpl
-    private lateinit var presenter: JoinGamePresenter
-
-    init {
-        view =
-            JoinGameViewImpl(
-                onJoinButtonClicked = {
-                    presenter.onJoinButtonPressed()
-                },
-                onBackButtonClicked = {
-                    presenter.returnToMenu()
-                },
-            )
-
-        presenter = JoinGamePresenter(view)
-
-        // Setup input handling
-        Gdx.input.inputProcessor =
-            object : InputAdapter() {
-                override fun touchDown(
-                    screenX: Int,
-                    screenY: Int,
-                    pointer: Int,
-                    button: Int,
-                ): Boolean {
-                    view.handleInput(screenX, screenY)
-                    return true
-                }
-
-                override fun keyTyped(character: Char): Boolean {
-                    view.handleKeyTyped(character)
-                    return true
-                }
-            }
-    }
-
-    override fun render(delta: Float) {
-        clearScreen(red = 0.1f, green = 0.1f, blue = 0.23f)
-        presenter.render()
-    }
-
-    override fun resize(
-        width: Int,
-        height: Int,
-    ) {
-        view.resize(width, height)
-    }
-
-    override fun dispose() {
-        view.closeView()
     }
 }
