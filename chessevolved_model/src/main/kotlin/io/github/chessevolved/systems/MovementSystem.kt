4180b62f9f9b36bd019072c0ab92e2b4dd8fb552
William Tjonndal <williaht@stud.ntnu.no>
2025-03-15 18:02:57 +0100

    build: fix kt linting

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/systems/MovementSystem.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/systems/MovementSystem.kt
@@ -5,8 +5,8 @@ import io.github.chessevolved.entities.ChessPiece
 import io.github.chessevolved.singletons.Mappers
 
 class MovementSystem {
-    fun moveChessPiece(chesspiece: ChessPiece, x:Int, y:Int) {
-        val position:PositionComponent = Mappers.getPosition(chesspiece)
+    fun moveChessPiece(chesspiece: ChessPiece, x: Int, y: Int) {
+        val position: PositionComponent = Mappers.getPosition(chesspiece)
         // 1 tile has a size of 32 by 32 pixels
         position.changePosition(x, y)
     }
