c5d178b7eca4867bd76be2fbb6da7a7e90278f41
CraZyB1336 <chrisbraat5@gmail.com>
2025-03-15 16:51:03 +0100

    feat: setup basic structure for MenuPresenter

---
+++ chessevolved_presenter/src/main/kotlin/io/github/chessevolved/presenters/MenuPresenter.kt
@@ -0,0 +1,15 @@
+package io.github.chessevolved.presenters
+
+class MenuPresenter : IPresenter {
+    override fun render() {
+        TODO("Not yet implemented")
+    }
+
+    fun enterJoinGame() {
+        // TODO: Append JoinGamePresenter to the Stack.
+    }
+
+    fun enterCreateGame() {
+        // TODO: Append CreateGamePresenter to the stack.
+    }
+}
