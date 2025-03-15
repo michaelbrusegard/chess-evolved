44d53cef71ab78f5fbf4bf3714ea531aae9d0681
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-15 20:17:44 +0100

    refactor: lobby structure

--- chessevolved_presenter/src/main/kotlin/io/github/chessevolved/presenters/JoinGamePresenter.kt
+++ chessevolved_presenter/src/main/kotlin/io/github/chessevolved/presenters/JoinGamePresenter.kt
@@ -1,11 +1,10 @@
 package io.github.chessevolved.presenters
 
-import io.github.chessevolved.systems.LobbySystem
+import io.github.chessevolved.singletons.Lobby
 import io.github.chessevolved.views.JoinGameView
 
 class JoinGamePresenter(
     private val view: JoinGameView,
-    private val lobbySystem: LobbySystem = LobbySystem(),
 ) : IPresenter {
     /**
      * Attempts to join a game lobby by ID
@@ -13,17 +12,13 @@ class JoinGamePresenter(
      * @param lobbyID String representing the lobby to join
      * @return Boolean indicating success or failure
      */
-    fun joinGame(lobbyID: String): Boolean {
-        println("JoinGamePresenter: Attempting to join lobby with ID: $lobbyID")
-
-        val success = lobbySystem.joinLobby(lobbyID)
+    fun joinGame(lobbyId: String): Boolean {
+        val success = Lobby.joinLobby(lobbyId)
 
         if (success) {
-            println("JoinGamePresenter: Successfully joined lobby $lobbyID")
             view.showJoinSuccess()
         } else {
-            println("JoinGamePresenter: Failed to join lobby $lobbyID")
-            view.showJoinError("Unable to join lobby: $lobbyID")
+            view.showJoinError("Unable to join lobby")
         }
 
         return success
@@ -32,7 +27,6 @@ class JoinGamePresenter(
     fun onJoinButtonPressed() {
         println("JoinGamePresenter: Join button pressed")
         val lobbyId = view.getLobbyId()
-        println("JoinGamePresenter: Retrieved lobby ID from view: '$lobbyId'")
         joinGame(lobbyId)
     }
 
@@ -41,15 +35,11 @@ class JoinGamePresenter(
      */
     fun returnToMenu() {
         println("JoinGamePresenter: Returning to menu")
-        view.closeView()
-        println("JoinGamePresenter: View closed")
         // TODO: idk but this should return here to let another presenter handle it
         // presenterManager.popToPresenter(MenuPresenter::class)
     }
 
     override fun render() {
-        println("JoinGamePresenter: Rendering join game view")
         view.displayLobbyInput()
-        println("JoinGamePresenter: Join game view displayed")
     }
 }
