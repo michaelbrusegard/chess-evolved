3d6c06901be33ac3bcce58cb34438ef5400d0861
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-15 17:07:20 +0100

    build: add ktlint for linting and formatting

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/components/PositionComponent.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/components/PositionComponent.kt
@@ -2,11 +2,17 @@ package io.github.chessevolved.components
 
 import com.badlogic.ashley.core.Component
 
-class PositionComponent(x: Int, y: Int) : Component{
+class PositionComponent(
+    x: Int,
+    y: Int,
+) : Component {
     var xPos: Int = x
     var yPos: Int = y
 
-    fun changePosition(x: Int, y: Int): Unit {
+    fun changePosition(
+        x: Int,
+        y: Int,
+    ) {
         xPos = x
         yPos = y
     }
