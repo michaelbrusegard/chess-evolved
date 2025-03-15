605a550c555ad7a03b41ab0f45e194b84c742839
Marius Waag Østro <mariuwos@stud.ntnu.no>
2025-03-15 18:29:09 +0100

    [state] start of a bigger gamestate object(again)

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/singletons/GameState.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/singletons/GameState.kt
@@ -1,4 +1,29 @@
 package io.github.chessevolved.singletons
 
-object GameState {
+
+enum class Piece {
+    WHITE_PAWN, WHITE_KING, WHITE_QUEEN, WHITE_ROOK, WHITE_BISHOP, WHITE_KNIGHT,
+    BLACK_PAWN, BLACK_KING, BLACK_QUEEN, BLACK_ROOK, BLACK_BISHOP, BLACK_KNIGHT
 }
+
+object GameState {
+
+}
+/*
+object GameState {
+    private var chessBoard = Array(8) { Array<Piece?>(8) { null } }
+
+    fun getChessBoard(): Array<Array<Piece?>> {
+        return chessBoard
+    }
+
+    fun setChessBoard(newBoard: Array<Array<Piece?>>) {
+        chessBoard = newBoard
+    }
+
+    fun updateChessBoard(updatedBoard: Array<Array<Piece?>>) {
+        chessBoard = updatedBoard
+    }
+
+}
+*/
