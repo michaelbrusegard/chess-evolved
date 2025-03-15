08c302d6c0bc9ba94211035b9eadea65fd701a96
William Tjonndal <williaht@stud.ntnu.no>
2025-03-15 18:01:59 +0100

    build: refactor MovementSystem.kt to use the Mappers singleton

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/systems/MovementSystem.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/systems/MovementSystem.kt
@@ -1,14 +1,12 @@
 package io.github.chessevolved.systems
 
-import com.badlogic.ashley.core.ComponentMapper
 import io.github.chessevolved.components.PositionComponent
 import io.github.chessevolved.entities.ChessPiece
+import io.github.chessevolved.singletons.Mappers
 
 class MovementSystem {
-    //TODO: refactor when the ComponentMapper class is created
     fun moveChessPiece(chesspiece: ChessPiece, x:Int, y:Int) {
-        val pm:ComponentMapper<PositionComponent> = ComponentMapper.getFor(PositionComponent::class.java)
-        val position:PositionComponent = pm.get(chesspiece)
+        val position:PositionComponent = Mappers.getPosition(chesspiece)
         // 1 tile has a size of 32 by 32 pixels
         position.changePosition(x, y)
     }
