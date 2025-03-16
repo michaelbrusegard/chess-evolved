73edeba839f41a8de18ed29c1f210c7bcf2b5162
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2025-03-16 14:54:30 +0100

    fix: issues with menuview and presenter when mergin with main

--- chessevolved_view/src/main/kotlin/io/github/chessevolved/views/MenuView.kt
+++ chessevolved_view/src/main/kotlin/io/github/chessevolved/views/MenuView.kt
@@ -2,16 +2,16 @@ package io.github.chessevolved.views
 
 import com.badlogic.gdx.graphics.g2d.Sprite
 
-class MenuView : IView {
-    override fun beginBatch() {
+class MenuView {
+    fun beginBatch() {
         TODO("Not yet implemented")
     }
 
-    override fun endBatch() {
+    fun endBatch() {
         TODO("Not yet implemented")
     }
 
-    override fun render(sprite: Sprite) {
+    fun render(sprite: Sprite) {
         TODO("Not yet implemented")
     }
 }
