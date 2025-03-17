d2db86e33a4dd7b1ec08b6fcabe7dd223d942211
mariusOstro <marius.w.ostro@gmail.com>
2025-03-17 20:40:18 +0100

    [state] gamestate object should now be  de-/serializable, i have had some issues with build.gradle and versions, but i think they are resolved, double check before PR approval

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/singletons/GameState.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/singletons/GameState.kt
@@ -1,27 +1,35 @@
 package io.github.chessevolved.singletons
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.*
+import kotlinx.serialization.json.*
 
-
+@Serializable
 enum class Piece {
     WHITE_PAWN, WHITE_KING, WHITE_QUEEN, WHITE_ROOK, WHITE_BISHOP, WHITE_KNIGHT,
     BLACK_PAWN, BLACK_KING, BLACK_QUEEN, BLACK_ROOK, BLACK_BISHOP, BLACK_KNIGHT
 }
 
+@Serializable
 enum class Ability {
     ABILITY1, ABILITY2, ABILITY3
 }
 
+@Serializable
 enum class WeatherEvent {
     WE1, WE2, WE3, WE4
 }
 
+@Serializable
 data class Position(val x: Int, val y: Int)
 
+@Serializable
 data class PieceState(
     val piece: Piece,
     var position: Position,
     val abilities: MutableList<Ability>
 )
 
+@Serializable
 data class TileState(
     val position: Position,
     val weatherEvents: MutableList<WeatherEvent> = mutableListOf()
@@ -34,6 +42,27 @@ object GameState {
             TileState(Position(x, y))
         }
     }
+
+    fun serialize(): String {
+        val snapshot = GameStateSnapshot(pieces, board.flatten())
+        return Json.encodeToString(snapshot)
+    }
+
+    fun deserialize(json: String) {
+        val snapshot = Json.decodeFromString<GameStateSnapshot>(json)
+        pieces.clear()
+        pieces.addAll(snapshot.pieces)
+
+        snapshot.boardTiles.forEach { tile ->
+            board[tile.position.x][tile.position.y] = tile
+        }
+    }
+
+    @Serializable
+    private data class GameStateSnapshot(
+        val pieces: List<PieceState>,
+        val boardTiles: List<TileState>
+    )
 }
 /*
 object GameState {
