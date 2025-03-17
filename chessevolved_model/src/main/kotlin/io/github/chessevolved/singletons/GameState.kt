abf499211386803771bf8d2fddb661bd62529a8e
mariusOstro <marius.w.ostro@gmail.com>
2025-03-17 19:47:01 +0100

    [state] gamestate object with pieces and board, still needs serialization

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/singletons/GameState.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/singletons/GameState.kt
@@ -6,8 +6,34 @@ enum class Piece {
     BLACK_PAWN, BLACK_KING, BLACK_QUEEN, BLACK_ROOK, BLACK_BISHOP, BLACK_KNIGHT
 }
 
-object GameState {
+enum class Ability {
+    ABILITY1, ABILITY2, ABILITY3
+}
 
+enum class WeatherEvent {
+    WE1, WE2, WE3, WE4
+}
+
+data class Position(val x: Int, val y: Int)
+
+data class PieceState(
+    val piece: Piece,
+    var position: Position,
+    val abilities: MutableList<Ability>
+)
+
+data class TileState(
+    val position: Position,
+    val weatherEvents: MutableList<WeatherEvent> = mutableListOf()
+)
+
+object GameState {
+    val pieces: MutableList<PieceState> = mutableListOf()
+    val board: Array<Array<TileState>> = Array(8) { x ->
+        Array(8) { y ->
+            TileState(Position(x, y))
+        }
+    }
 }
 /*
 object GameState {
