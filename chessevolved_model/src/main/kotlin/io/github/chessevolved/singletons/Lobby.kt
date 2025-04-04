20da0b58b91ac66a1d835c96c8aa8076b7488c4d
Sivert Underdal <sivertun@stud.ntnu.no>
2025-04-04 14:15:27 +0200

    feature: implement necessary logic in Lobby singleton to manage lobbies in supabase

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/singletons/Lobby.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/singletons/Lobby.kt
@@ -1,23 +1,71 @@
 package io.github.chessevolved.singletons
 
+import com.badlogic.gdx.utils.Array
+import io.github.chessevolved.singletons.supabase.SupabaseLobbyHandler
+import io.github.chessevolved.singletons.supabase.SupabaseLobbyHandler.Lobby
+import kotlin.reflect.KFunction1
+
 object Lobby {
-    var lobbyId: String? = null
-    // This needs to do more things but I dont remember all the code from the supabase setup
+    private var lobbyId: String? = null
+    private var subscribers = Array<KFunction1<Lobby, Unit>>()
 
-    fun joinLobby(lobbyId: String): Boolean {
-        // Network code would eventually go here
-        // For now, just validate the ID format
-        println("Lobby: Joining lobby with ID: $lobbyId")
-        return true
+    suspend fun joinLobby(lobbyId: String): Boolean {
+        println("Lobby: Joining lobby with ID: $lobbyId...")
+        try {
+            SupabaseLobbyHandler.joinLobby(lobbyId, ::onLobbyRowUpdate)
+            this.lobbyId = lobbyId
+            return true
+        } catch (e: Exception) {
+            // TODO: Elevate exception with appropriate message.
+            return false
+        }
     }
 
-    fun createLobby(): Boolean {
-        // Will create a new lobby and store id in lobby state
-        return true
+    suspend fun createLobby(): Boolean {
+        try {
+            val lobbyId = SupabaseLobbyHandler.createLobby(::onLobbyRowUpdate)
+            this.lobbyId = lobbyId
+            return true
+        } catch (e: Exception) {
+            // TODO: Elevate exception with appropriate message.
+            return false
+        }
     }
 
-    fun isInLobby(): Boolean {
-        // if the player is in a lobby
-        return true
+    suspend fun setLobbySettings() {
+        if (lobbyId == null) {
+            throw IllegalStateException("Can't update game settings when not in a lobby!")
+        }
+        SupabaseLobbyHandler.updateLobbySettings(lobbyId!!, GameSettings.getGameSettings())
+    }
+
+    suspend fun startGame() {
+        if (lobbyId == null) {
+            throw IllegalStateException("Can't start game when not in a lobby!")
+        }
+        try {
+            SupabaseLobbyHandler.startGame(lobbyId!!, GameSettings.getGameSettings())
+        } catch (e: Exception) {
+            // TODO: Elevate exception with appropriate message
+        }
+    }
+
+    fun isInLobby(): Boolean = !lobbyId.isNullOrEmpty()
+
+    fun getLobbyId(): String? = lobbyId
+
+    private fun onLobbyRowUpdate(lobby: SupabaseLobbyHandler.Lobby) {
+        subscribers.forEach {
+            it.invoke(lobby)
+        }
+    }
+
+    fun subscribeToLobbyUpdates(onEventListener: KFunction1<Lobby, Unit>) {
+        subscribers.add(onEventListener)
+    }
+
+    fun unsubscribeFromLobbyUpdates() {
+        // TODO: Might have to change up implementation of subscribing to make it possible to find which function to remove from the subscribed-arraylist.
+        // Maybe switch out the array-list with a HashMap, and pass class-name to subscribe-function, so you can unsubscribe by passing class-name.
     }
 }
