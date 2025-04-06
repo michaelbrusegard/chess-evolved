e98843a38049965214a7caf6305bed5f57513f80
William Tjonndal <williaht@stud.ntnu.no>
2025-04-07 00:53:27 +0200

    feature: Update abilityInventory and descriptionInventory with ability names and descriptions

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/systems/AbilitySystem.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/systems/AbilitySystem.kt
@@ -6,9 +6,28 @@ import io.github.chessevolved.entities.ChessPiece
 import io.github.chessevolved.singletons.Mappers
 import ktx.assets.toInternalFile
 
-class AbilitySystem (
-    private var abilityInventory: ArrayList<String>,
-    private var abilityDescriptionInventory: HashMap<String, String>
+class AbilitySystem(
+    private var abilityInventory: List<String> = listOf("explosion", "conscription", "shadow_step",
+     "earthquake", "shield", "aura_of_terror", "royal_guard", "short_range_teleport", "checkers"),
+    private var abilityDescriptionInventory: HashMap<String, String> = hashMapOf(
+        "An explosion erupts on the chosen tile, and all adjacent tiles excluding diagonal tiles," +
+            " destroying all pieces and clearing all weather events." to "explosion",
+        "Summon a permanent friendly pawn in an empty adjacent empty tile." to "conscription",
+        "Teleport on the other side of an adjacent piece." to "shadow_step",
+        "Make a move as normal with this piece. When moving into the new chosen tile, stun all" +
+            " adjacent enemy pieces for 1 turn." to "earthquake",
+        "Protect a piece from harm, such as being taken by other pieces or abilities. Lasts for 2" +
+            " turns or until the shield blocks an attack." to "shield",
+        "Enemy pieces may not move adjacent to this piece. Enemy pieces already adjacent to this" +
+            " piece are not affected. Lasts for 2 turns or until the piece is destroyed."
+            to "aura_of_terror",
+        "This piece cannot be taken or destroyed, as long as it is adjacent to its king piece. " +
+            "Lasts for 2 turns or until the piece is destroyed." to "royal_guard",
+        "At the start of your turn, this piece will randomly teleport in a adjacent tile. Lasts" +
+            " for 3 turns or until the piece is destroyed." to "short_range_teleport",
+        "Transform this piece into a checkers piece, making it follow checkers rules instead of" +
+            " chess rules. This effect is permanent." to "checkers"
+    )
 ) {
     /**
      * @param ability - ability name as a string in lowercase
@@ -41,7 +60,7 @@ class AbilitySystem (
         }
         if (abilityName.equals("conscription")) {
             //TODO: implement logic for the conscription ability
-            // Summon a permanent friendly pawn in an adjacent empty tile.
+            // Summon a permanent friendly pawn in an empty adjacent empty tile.
         }
         if (abilityName.equals("shadow_step")) {
             //TODO: implement logic for the shadow step ability
@@ -81,7 +100,7 @@ class AbilitySystem (
     }
 
     /** @return - Entire list of active abilities */
-    fun getAbilityInventory(): ArrayList<String> {
+    fun getAbilityInventory(): List<String> {
         return abilityInventory
     }
 
