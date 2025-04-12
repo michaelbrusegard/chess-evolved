fa3a44f2b397ff1985f99a716718a54b08aaa7da
Sivert Underdal <sivertun@stud.ntnu.no>
2025-04-12 18:18:35 +0200

    feature: add gameStage and uiStage as two stages in GameView

--- chessevolved_view/src/main/kotlin/io/github/chessevolved/views/GameView.kt
+++ chessevolved_view/src/main/kotlin/io/github/chessevolved/views/GameView.kt
@@ -1,55 +1,64 @@
 package io.github.chessevolved.views
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.InputMultiplexer
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.ScreenViewport
 
 fun interface OnPieceClickedListener {
-    fun onClick(x: Int, y: Int)
+    fun onClick(
+        x: Int,
+        y: Int,
+    )
 }
 
 class GameView : IView {
-    private lateinit var stage: Stage
+    private lateinit var gameStage: Stage
+    private lateinit var uiStage: Stage
 
     private val gameBatch = SpriteBatch()
 
     private var clickListener: OnPieceClickedListener? = null
 
-    private val screenViewport = ScreenViewport()
+    private val gameViewport = ScreenViewport()
+    private val uiViewport = ScreenViewport()
+
+    private val inputMultiplexer = InputMultiplexer()
 
     override fun init() {
-        stage = Stage(screenViewport)
+        gameStage = Stage(gameViewport, gameBatch)
+        uiStage = Stage(uiViewport, gameBatch)
     }
 
     fun getGameBatch(): SpriteBatch = gameBatch
 
-    fun getStage(): Stage = stage
+    fun getStage(): Stage = gameStage
 
     fun setOnPieceClickedListener(listener: OnPieceClickedListener) {
         this.clickListener = listener
     }
 
     override fun render() {
-        stage.viewport = screenViewport
-        stage.act(Gdx.graphics.deltaTime)
-        stage.draw()
+        gameStage.viewport = gameViewport
+        gameStage.act(Gdx.graphics.deltaTime)
+        gameStage.draw()
     }
 
     override fun resize(
         width: Int,
         height: Int,
     ) {
-        stage.viewport.update(width, height, true)
+        gameStage.viewport.update(width, height, true)
     }
 
     override fun dispose() {
-        stage.dispose()
+        gameStage.dispose()
         gameBatch.dispose()
     }
 
     override fun setInputProcessor() {
-        Gdx.input.inputProcessor = stage
+        Gdx.input.inputProcessor = gameStage
     }
 
     fun getClickListener(): OnPieceClickedListener? = clickListener
