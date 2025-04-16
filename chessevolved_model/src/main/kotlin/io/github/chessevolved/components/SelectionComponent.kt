24485ad8e6c2cd293d7cf24bd4b6e97db49305de
CraZyB1336 <chrisbraat5@gmail.com>
2025-04-16 21:09:14 +0200

    feat: add component mapper to selectioncomponent

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/components/SelectionComponent.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/components/SelectionComponent.kt
@@ -1,5 +1,11 @@
 package io.github.chessevolved.components
 
 import com.badlogic.ashley.core.Component
+import com.badlogic.ashley.core.ComponentMapper
 
-class SelectionComponent : Component
+class SelectionComponent : Component {
+    companion object {
+        val mapper: ComponentMapper<SelectionComponent> =
+            ComponentMapper.getFor(SelectionComponent::class.java)
+    }
+}
