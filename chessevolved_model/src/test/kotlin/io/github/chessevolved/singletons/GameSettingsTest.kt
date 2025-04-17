b06b04bdb0d17e57d7c057e1bfcfadd4d0b3500c
William Tjonndal <williaht@stud.ntnu.no>
2025-04-17 15:08:21 +0200

    build: create GameSettingsTest.kt

---
+++ chessevolved_model/src/test/kotlin/io/github/chessevolved/singletons/GameSettingsTest.kt
@@ -0,0 +1,32 @@
+package io.github.chessevolved.singletons
+
+import io.github.chessevolved.shared.SettingsDTO
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+class GameSettingsTest {
+
+    @Test
+    fun isFOWEnabled() {
+        assertEquals(false, GameSettings.isFOWEnabled())
+    }
+
+    @Test
+    fun getBoardSize() {
+        assertEquals(8, GameSettings.getBoardSize())
+    }
+
+    @Test
+    fun getGameSettings() {
+        val settingsDTO: SettingsDTO = SettingsDTO(false, 8)
+        assertEquals(settingsDTO, GameSettings.getGameSettings())
+    }
+
+    @Test
+    fun setGameSettings() {
+        val settingsDTO: SettingsDTO = SettingsDTO(true, 16)
+        GameSettings.setGameSettings(settingsDTO)
+        assertEquals(settingsDTO, GameSettings.getGameSettings())
+    }
+}
