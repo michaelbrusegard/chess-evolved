1ecba180306a026561056a824cc5ed7fb5d86669
Sivert Underdal <sivertun@stud.ntnu.no>
2025-04-19 01:31:27 +0200

    feature: add AbilityItemFactory and add method to InputSystem for handling clicking on AbilityCards

--- chessevolved_model/src/main/kotlin/io/github/chessevolved/systems/InputSystem.kt
+++ chessevolved_model/src/main/kotlin/io/github/chessevolved/systems/InputSystem.kt
@@ -3,6 +3,7 @@ package io.github.chessevolved.systems
 import com.badlogic.ashley.core.Entity
 import com.badlogic.ashley.core.Family
 import com.badlogic.ashley.systems.IteratingSystem
+import io.github.chessevolved.components.AbilityCardComponent
 import io.github.chessevolved.components.CanBeCapturedComponent
 import io.github.chessevolved.components.CapturedComponent
 import io.github.chessevolved.components.ClickEventComponent
@@ -14,9 +15,10 @@ import io.github.chessevolved.components.SelectionComponent
 import io.github.chessevolved.components.WeatherEventComponent
 import io.github.chessevolved.singletons.ECSEngine
 
-class InputSystem : IteratingSystem(
-    Family.all(ClickEventComponent::class.java).get(),
-) {
+class InputSystem :
+    IteratingSystem(
+        Family.all(ClickEventComponent::class.java).get(),
+    ) {
     override fun processEntity(
         entity: Entity?,
         deltaTime: Float,
@@ -25,6 +27,8 @@ class InputSystem : IteratingSystem(
             handlePieceClicked(entity)
         } else if (entity?.getComponent(WeatherEventComponent::class.java) != null) {
             handleBoardSquareClicked(entity)
+        } else if (entity?.getComponent(AbilityCardComponent::class.java) != null) {
+            handleAbilityCardClicked(entity)
         }
 
         entity?.remove(ClickEventComponent::class.java)
@@ -48,15 +52,23 @@ class InputSystem : IteratingSystem(
     private fun handleBoardSquareClicked(boardSquare: Entity) {
         val position = PositionComponent.mapper.get(boardSquare).position
 
-        ECSEngine.getEntitiesFor(Family.all(PieceTypeComponent::class.java, SelectionComponent::class.java).get())
-            .firstOrNull()?.add(MovementIntentComponent(position))
+        ECSEngine
+            .getEntitiesFor(Family.all(PieceTypeComponent::class.java, SelectionComponent::class.java).get())
+            .firstOrNull()
+            ?.add(MovementIntentComponent(position))
+    }
+
+    private fun handleAbilityCardClicked(abilityCard: Entity) {
+        println("Ability card clicked")
+        // TODO: Give selected component, de-select all other ability-cards, set highlightComponent to white (selected color)
     }
 }
 
-class InputService() {
+class InputService {
     fun clickPieceAtPosition(position: Position) {
         val entity =
-            ECSEngine.getEntitiesFor(Family.all(PieceTypeComponent::class.java).get())
+            ECSEngine
+                .getEntitiesFor(Family.all(PieceTypeComponent::class.java).get())
                 .find { PositionComponent.mapper.get(it).position == position }
 
         entity?.add(ClickEventComponent())
@@ -64,8 +76,18 @@ class InputService() {
 
     fun clickBoardSquareAtPosition(position: Position) {
         val entity =
-            ECSEngine.getEntitiesFor(Family.all(WeatherEventComponent::class.java).get())
+            ECSEngine
+                .getEntitiesFor(Family.all(WeatherEventComponent::class.java).get())
                 .find { PositionComponent.mapper.get(it).position == position }
+
+        entity?.add(ClickEventComponent())
+    }
+
+    fun clickAbilityCardWithId(abilityCardId: Int) {
+        val entity =
+            ECSEngine
+                .getEntitiesFor(Family.all(AbilityCardComponent::class.java).get())
+                .find { AbilityCardComponent.mapper.get(it).id == abilityCardId }
 
         entity?.add(ClickEventComponent())
     }
