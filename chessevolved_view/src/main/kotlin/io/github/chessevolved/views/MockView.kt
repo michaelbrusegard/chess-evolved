a3e7224e408fdfac09c6453f1e22ab11360ad5c6
William Tjonndal <williaht@stud.ntnu.no>
2025-04-20 14:31:50 +0200

    build: Create MockView.kt

---
+++ chessevolved_view/src/main/kotlin/io/github/chessevolved/views/MockView.kt
@@ -0,0 +1,20 @@
+package io.github.chessevolved.views
+
+class MockView(
+    private val lobbyCode: String,
+) : IView {
+    override fun init() {
+    }
+
+    override fun render() {
+    }
+
+    override fun resize(width: Int, height: Int) {
+    }
+
+    override fun dispose() {
+    }
+
+    override fun setInputProcessor() {
+    }
+}
